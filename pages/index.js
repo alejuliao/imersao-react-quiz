8e5dad85ffa1ca7e0d9949666d3bb4d01b9ec40a
alejuliao <alexand.juliao@gmail.com>
2021-01-30 17:21:31 -0300

    last day

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
+import { motion } from 'framer-motion';
+import Link from '../src/components/Link';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
@@ -38,11 +40,23 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title> Puzzle Quiz</title>
+        <title>
+          AluraQuiz -
+          {db.title}
+        </title>
       </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
@@ -67,7 +81,6 @@ export default function Home() {
               placeholder="Digite seu nome"
               value={name}
               />
-              
             <Button type="submit" disabled={name.length === 0}>
               {`Jogar ${name}`}
             </Button>
@@ -78,14 +91,50 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>          </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer 
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/omariosouto" />
     </QuizBackground>
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-
 const InputBase = Styled.input`
   width: 100%;
   padding: 15px;
@@ -13,27 +12,32 @@ const InputBase = Styled.input`
   border-radius: ${({ theme }) => theme.borderRadius};
   outline: 0;
   margin-bottom: 25px;
+  ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: ${({ theme }) => theme.colors.contrastText}DD;
+    opacity: 1; /* Firefox */
+  }
+
 `;
 
-export default function Input({onChange, placeholder, ...props}){
-    return ( 
-        <div>
-            <InputBase 
-                placeholder={placeholder}
-                onChange={onChange}
-                {...props}
-            />
-        </div>
-    )
+export default function Input({ onChange, placeholder, ...props }) {
+  return (
+    <div>
+      <InputBase
+        placeholder={placeholder}
+        onChange={onChange}
+        {...props}
+      />
+    </div>
+  );
 }
 
 Input.defaultProps = {
-    value: '',
+  value: '',
 };
 
 Input.propTypes = {
-    onChange:PropTypes.func.isRequired,
-    placeholder: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
 };
